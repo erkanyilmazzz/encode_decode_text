e5a704a6b5653ba9120456636d353a33dd7edfcf
erkan yılmaz <33496216+erkanyilmazzz@users.noreply.github.com>
2019-10-31 17:04:58 +0300

    Add files via upload
    last version

--- decode.lisp
+++ decode.lisp
@@ -1,12 +1,11 @@
 ; *********************************************
 ; *  341 Programming Languages                *
 ; *  Fall 2019                                *
-; *  Author: Erkan Yılmaz                     *
+; *  Author: Yakup Genc                       *
 ; *********************************************
 
 ;; utility functions 
 (load "include.lisp") ;; "c2i and "i2c"
-
 
 
 (defun read-as-list (filename)
@@ -14,72 +13,15 @@
 
 	; Implement this function...
 
-	(format t "~%")
-	(setq words '( ))
-
-				(setq words '( ))
-         		(setq word '( ))	
-         		(setq l '())
-
-
- 	(with-open-file (infile filename
-                              :direction :input)
-       (do (
-       			(sym 
-       				(read-char  infile nil 'eof) 
-       				(read-char  infile nil 'eof)
-       			)
-
-       		)
-         		((eql sym 'eof) 'done)
-         		(when (or (char= sym #\Space) (char= sym #\Newline))
-
-         			(nreverse word )
-         			(push word words)	
-         			(setq word '())
-         		)
-         		(when (and (not (char= sym #\Space)) (not (char= sym #\Newline)))
-         			(setq word (push  sym word))
-         		)
-        )
-       (close infile)
-    )
- 	(nreverse words)
 )
+
 ;; -----------------------------------------------------
 ;; HELPERS
 ;; *** PLACE YOUR HELPER FUNCTIONS BELOW ***
 
-(defun spell-checker-0 (word dic)
+(defun spell-checker-0 (word)
 	;you should implement this function
-
-
-;	(cond  ( (equal word (car dic))   t   )
-;		   ( (eq    (car dic) nil )	 nil  )
-;		   ((spell-checker-0 word (cdr dic)))
-;	)
-
-(dolist (n dic)
-	(cond ((equal n word)    (return-from spell-checker-0 t))
-	)
 )
-
-nil
-)
-
-
-(defun spell-checker-0_unit_test()
-
-	(let ((doc (read-as-list "dictionary2.txt")))
-		;(format t "~a" doc)
-		(setq word '(#\t #\h #\i #\s ))
-		(cond ((spell-checker-0 word doc)  (print "buldu"))
-			((print "bulamadı"))	
-		)
-
-	)
-)
-
 
 (defun spell-checker-1 (word)
  	;you should implement this function
@@ -89,109 +31,8 @@ nil
 ;; -----------------------------------------------------
 ;; DECODE FUNCTIONS
 
-(defun add-pair (l1 p1 p2 )
-    (setq l1 (cons (cons p1 p2) l1) )
-
-)
-
-(defun add-pair_unit_test ()
-	(setq l '())
-
-	(setq l (add-pair l 1 2))
-	(setq l (add-pair l 5 2))
-	(setq l (add-pair l 1 4))
-	(setq l (add-pair l 7 2))
-
-	(print l)
-)
-
-
-;;returns pair if there is stack over flow back to  this
-(defun matcher (l dict words)
-	(setq pairs '())
-	(setq abc '(#\a #\b #\c #\d #\e #\f #\g #\h #\i #\j 
-				#\k #\l #\m #\n #\o #\p #\q #\r #\s #\t 
-				#\u #\v #\w #\x #\y #\z ))
-  (dotimes (n 26)
-    (setq pairs (add-pair pairs (car abc) (car l)))
-    (setq abc (cdr abc))
-    (setq l   (cdr l  ))
-  )
-  ;;generate pairs
-
-  
-
-
-
-
-
-
-
-  (format t   "~a~%~%" pairs)
-
-
-
-
-)
-
-
-(defun change_word (word pairs)
-  ;(format t  "~a" pairs)
-
-  (format t   "~a ~%" pairs)
-  (format t   "~a ~%" word)
-
-  (setq changed_word '())
-
-  (loop for i in word 
-   do(setq changed_word (push   i changed_word))
-            
-    do(loop for j in pairs
-        do(if (eql i (car j))
-          (progn
-            (setq changed_word (remove  i changed_word))
-            (setq changed_word (push   (cdr j) changed_word))
-            )
-             
-
-
-          )
-
-    ;do(format t   "~a ~a ~%" i (car j))
-    
-
-    
-    )
-  )
-  (setq changed_word (nreverse changed_word))
-  (format t "~a " changed_word)
-)
-(defun change_word_unit_test ()
-  (setq pairs '())
-  
-  (setq pairs (add-pair pairs #\t #\a))
-  (setq pairs (add-pair pairs #\h #\b))
-  (setq pairs (add-pair pairs #\i #\c))
-  (setq pairs (add-pair pairs #\s #\d))
-
-  (setq word '( #\a #\d #\s #\t #\h #\i #\s #\c))
-
-
-  (change_word word pairs)
-
-)
-
-
-
-(defun decode (l words )
-
-
-)
-
-
 (defun Gen-Decoder-A (paragraph)
 	;you should implement this function
-
 )
 
 (defun Gen-Decoder-B-0 (paragraph)
@@ -205,6 +46,7 @@ nil
 (defun Code-Breaker (document decoder)
   	;you should implement this function
 )
+
 ;; -----------------------------------------------------
 ;; Test code...
 
@@ -212,54 +54,11 @@ nil
 	(print "....................................................")
 	(print "Testing ....")
 	(print "....................................................")
-	(let ((doc (read-as-list "document1.txt")))
+	(let (doc (read-as-list "document1.txt"))
 		(print doc)
-		;(print (read-as-list "document2.txt"))
-
 	)
-
-
 )
-
-
-(defun map-permutations (fun lst  dict words )
-  (if (null lst) (funcall fun nil dict words)
-    (map nil
-       (lambda (x)
-         (map-permutations 
-          (lambda (l dict words) (funcall fun (cons x l) dict words)) 
-          (remove x lst) dict words))
-       lst))
- )
-
-
-(defun print-list (l1)
-	(print l1)
-)
-
-(defun map-permutations_unit_test ()
-
-  (setq dict  (read-as-list "dictionary2.txt"))
-  (setq words (read-as-list "text.txt" ))
-;  (format t  "~a " words)
-
-	(map-permutations #'matcher  '(#\a #\b #\c #\d #\e #\f #\g #\h #\i #\j #\k #\l #\m #\n #\o #\p #\q #\r
-				#\r #\s #\t #\u #\v #\w #\x #\y #\z )   dict  words)
-
-)
-
-
-
 
 
 ;; test code...
-;(test_on_test_data)
-;(spell-checker-0_unit_test)
-;(map-permutations_unit_test )
-;(add-pair_unit_test)
-;(change_word_unit_test)
-;;(setq list1 (append list1 (list '(erkan))))
-;;(print list1)
-
-;(all-permutations_unit_test)
-
+(test_on_test_data)
